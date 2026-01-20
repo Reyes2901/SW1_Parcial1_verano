d01d4e31136f14b33885063587b05c23e37ff4ee
Reyes2901 <reyesponce78@gmail.com>
2026-01-20 10:50:39 -0600

    fix: eliminar duplicado de import

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -13,9 +13,6 @@ const pool = new Pool({
 export default pool;
 */
 
-import pkg from 'pg';
-const { Pool } = pkg;
-
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false } // necesario en Railway
