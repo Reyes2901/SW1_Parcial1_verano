756b4027d8e931772e18c87359e5bdc440676d12
Reyes2901 <reyesponce78@gmail.com>
2026-01-20 11:33:44 -0600

    fix: error en config

--- backend/src/config.js
+++ backend/src/config.js
@@ -2,6 +2,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+/*
 export const TOKEN_SECRET = process.env.TOKEN_SECRET || 'secret123Guitar.'
 
 // Aceptar por defecto los puertos de desarrollo más comunes (Vite 5173, 5000, 3000)
@@ -9,3 +10,9 @@ export const TOKEN_SECRET = process.env.TOKEN_SECRET || 'secret123Guitar.'
 const frontendUrlsString = process.env.FRONTEND_URL || 'http://localhost:5173,http://localhost:5000,http://localhost:3000';
 export const FRONTEND_URLS = frontendUrlsString.split(',').map(url => url.trim());
 export const FRONTEND_URL = FRONTEND_URLS[0];
+*/
+
+export const TOKEN_SECRET = process.env.TOKEN_SECRET || 'secret123Guitar.';
+
+const frontendUrlsString = process.env.FRONTEND_URLS || 'http://localhost:5173,http://localhost:5000,http://localhost:3000';
+export const FRONTEND_URLS = frontendUrlsString.split(',').map(url => url.trim());
